71daa45c0b0f0513c24461a0a24a277bc7430f91
Eric Ludlam <eric@siege-engine.com>
2020-12-21 21:26:19 -0500

    matlab-complete.el:
    Updated to support `completion-at-point' instead of using home-grown completion stuff.
    (matlab-completion-at-point-function)
    (matlab-completion-at-point-function-shell)
    (matlab-completion-at-point-function-local): New.  Derived from...
    (matlab-complete-symbol)
    (matlab-complete-symbol-with-shell)
    (matlab-complete-symbol-local): Removed.
    (matlab-last-prefix, matlab-last-semantic, matlab-completion-search-state):
    Removed
    (matlab-find-recent-variable, matlab-find-user-functions)
    (matlab-generic-list-expand, matlab-solo-completions)
    (matlab-value-completions, matlab-property-completions):
    Remove optional argument 'next' functionality.
    (matlab-completions-local-only): New.

--- matlab-complete.el
+++ matlab-complete.el
@@ -19,19 +19,47 @@
 
 ;;; Commentary:
 ;;
-;; This library supports a simple completion technique for MATLAB.
+;; Support completion in MATLAB buffers.
 ;;
-;; It uses the following techniques:
-;; * Lists of symbols that can be completed (from ~ 2004ish)
-;; * scan buffer for functions and symbols.
-;; * look for local files that might be functions.
+;; If `matlab-shell' is active, use the subprocess to perform completions.
 ;;
-;; Moved to separate library, as some modern tools do a better job
-;; and this can be loaded optionally.
+;; Otherwise, use `matlab-complete-symbol-local' found in matlab-complete-local.
+;; The local version is an old implementation with minimal completion support.
 
 ;;; Code:
 (require 'matlab)
 
+
+;;;###autoload
+(defun matlab-completion-at-point-function ()
+  "Function to be used with `completion-at-point-functions'.
+Returns list of completions discovered at point."
+  (if (and (featurep 'matlab-shell) (matlab-shell-active-p) matlab-shell-ask-MATLAB-for-completions)
+      ;; Use MATLAB shell if active and asking for completions is enabled.
+      (matlab-completion-at-point-function-shell)
+    ;; Else, do the antique version.
+    (matlab-completion-at-point-function-local)
+    ))
+
+(defun matlab-completion-at-point-function-shell()
+  "Function used with `matlab-completion-at-point-function' when `matlab-shell' is active."
+  (matlab-navigation-syntax
+    (let* ((common-substr-start-pt nil)
+	   (common-substr-end-pt nil)
+	   (prefix (if (member (preceding-char) '(?  ?\t ?\n ?, ?\( ?\[ ?\'))
+		       ""
+		     (buffer-substring-no-properties
+		      (save-excursion (forward-word -1) (setq common-substr-start-pt (point)))
+		      (setq common-substr-end-pt (point)))))
+	   (completion-info (matlab-shell-completion-list prefix))
+           (completions-shell (cdr (assoc 'completions completion-info)))
+	   (completions-local (matlab-completions-local-only prefix))
+	   )
+      (list common-substr-start-pt common-substr-end-pt (append completions-shell completions-local))
+      )))
+
+;;; OLD Completion Tools
+;; ---------------------------------------------------------
 ;;; Customizations ===========================================================
 ;;
 (defcustom matlab-completion-technique 'complete
@@ -251,22 +279,16 @@ If the list is empty, then searches continue backwards through the code."
 (defvar matlab-most-recent-variable-list nil
   "Maintained by `matlab-find-recent-variable'.")
 
-(defun matlab-find-recent-variable (prefix &optional next)
+(defun matlab-find-recent-variable (prefix)
   "Return the most recently used variable starting with PREFIX as a string.
-See `matlab-find-recent-variable-list' for details.
-In NEXT is non-nil, than continue through the list of elements."
-  (if next
-      (let ((next (car matlab-most-recent-variable-list)))
-	(setq matlab-most-recent-variable-list
-	      (cdr matlab-most-recent-variable-list))
-	next)
-    (let ((syms (matlab-find-recent-variable-list prefix))
-	  (first nil))
-      (if (eq matlab-completion-technique 'complete)
-	  syms
-	(setq first (car syms))
-	(setq matlab-most-recent-variable-list (cdr syms))
-	first))))
+See `matlab-find-recent-variable-list' for details."
+  (let ((syms (matlab-find-recent-variable-list prefix))
+	(first nil))
+    (if (eq matlab-completion-technique 'complete)
+	syms
+      (setq first (car syms))
+      (setq matlab-most-recent-variable-list (cdr syms))
+      first)))
 
 (defun matlab-find-user-functions-list (prefix)
   "Return a list of user defined functions that match PREFIX."
@@ -303,67 +325,50 @@ In NEXT is non-nil, than continue through the list of elements."
 (defvar matlab-user-function-list nil
   "Maintained by `matlab-find-user-functions'.")
 
-(defun matlab-find-user-functions (prefix &optional next)
-  "Return a user function that match PREFIX and return it.
-If optional argument NEXT is non-nil, then return the next found
-object."
-  (if next
-      (let ((next (car matlab-user-function-list)))
-	(setq matlab-user-function-list (cdr matlab-user-function-list))
-	next)
-    (let ((syms (matlab-find-user-functions-list prefix))
-	  (first nil))
-      (if (eq matlab-completion-technique 'complete)
-	  syms
-	(setq first (car syms))
-	(setq matlab-user-function-list (cdr syms))
-	first))))
+(defun matlab-find-user-functions (prefix)
+  "Return a user function that match PREFIX and return it."
+  (let ((syms (matlab-find-user-functions-list prefix))
+	(first nil))
+    (if (eq matlab-completion-technique 'complete)
+	syms
+      (setq first (car syms))
+      (setq matlab-user-function-list (cdr syms))
+      first)))
 
 (defvar matlab-generic-list-placeholder nil
   "Maintained by `matlab-generic-list-expand'.
 Holds sub-lists of symbols left to be expanded.")
 
-(defun matlab-generic-list-expand (list prefix &optional next)
-  "Return an element from LIST that start with PREFIX.
-If optional NEXT argument is non nil, then the next element in the
-list is used.  nil is returned if there are not matches."
-  (if next
-      (let ((next (car matlab-generic-list-placeholder)))
-	(setq matlab-generic-list-placeholder
-	      (cdr matlab-generic-list-placeholder))
-	next)
-    (let ((re (concat "^" (regexp-quote prefix)))
-	  (first nil)
-	  (fl nil))
-      (while list
-	(if (string-match re (car list))
-	    (setq fl (cons (car list) fl)))
-	(setq list (cdr list)))
-      (setq fl (nreverse fl))
-      (if (eq matlab-completion-technique 'complete)
-	  fl
-	(setq first (car fl))
-	(setq matlab-generic-list-placeholder (cdr fl))
-	first))))
+(defun matlab-generic-list-expand (list prefix)
+  "Return an element from LIST that start with PREFIX."
+  (let ((re (concat "^" (regexp-quote prefix)))
+	(first nil)
+	(fl nil))
+    (while list
+      (if (string-match re (car list))
+	  (setq fl (cons (car list) fl)))
+      (setq list (cdr list)))
+    (setq fl (nreverse fl))
+    (if (eq matlab-completion-technique 'complete)
+	fl
+      (setq first (car fl))
+      (setq matlab-generic-list-placeholder (cdr fl))
+      first)))
 
-(defun matlab-solo-completions (prefix &optional next)
-  "Return PREFIX matching elements for solo symbols.
-If NEXT then the next patch from the list is used."
-  (matlab-generic-list-expand matlab-keywords-solo prefix next))
+(defun matlab-solo-completions (prefix)
+  "Return PREFIX matching elements for solo symbols."
+  (matlab-generic-list-expand matlab-keywords-solo prefix))
 
-(defun matlab-value-completions (prefix &optional next)
-  "Return PREFIX matching elements for value symbols.
-If NEXT then the next patch from the list is used."
-  (matlab-generic-list-expand matlab-keywords-return prefix next))
+(defun matlab-value-completions (prefix)
+  "Return PREFIX matching elements for value symbols."
+  (matlab-generic-list-expand matlab-keywords-return prefix))
 
-(defun matlab-boolean-completions (prefix &optional next)
-  "Return PREFIX matching elements for boolean symbols.
-If NEXT then the next patch from the list is used."
-  (matlab-generic-list-expand matlab-keywords-boolean prefix next))
+(defun matlab-boolean-completions (prefix)
+  "Return PREFIX matching elements for boolean symbols."
+  (matlab-generic-list-expand matlab-keywords-boolean prefix))
 
-(defun matlab-property-completions (prefix &optional next)
-  "Return PREFIX matching elements for property names in strings.
-If NEXT then the next property from the list is used."
+(defun matlab-property-completions (prefix)
+  "Return PREFIX matching elements for property names in strings."
   (let ((f (matlab-function-called-at-point))
 	(lst matlab-property-lists)
 	(foundlst nil)
@@ -377,7 +382,7 @@ If NEXT then the next property from the list is used."
     (if foundlst
 	(setq foundlst (append foundlst matlab-core-properties))
       (setq foundlst matlab-all-known-properties))
-    (setq expandto (matlab-generic-list-expand foundlst prefix next))
+    (setq expandto (matlab-generic-list-expand foundlst prefix))
     ;; This looks to see if we have a singular completion.  If so,
     ;; then return it, and also append the "'" to the end.
     (cond ((and (listp expandto) (= (length expandto) 1))
@@ -386,52 +391,10 @@ If NEXT then the next property from the list is used."
 	   (setq expandto (concat expandto "'"))))
     expandto))
 
-(defvar matlab-last-prefix nil
-  "Maintained by `matlab-complete-symbol'.
-The prefix used for the first completion command.")
-(defvar matlab-last-semantic nil
-  "Maintained by `matlab-complete-symbol'.
-The last type of semantic used while completing things.")
-(defvar matlab-completion-search-state nil
-  "List of searching things we will be doing.")
+(defun matlab-completion-at-point-function-local ()
+  "Function used with `matlab-completion-at-point-function' with no shell
 
-;;;###autoload
-(defun matlab-complete-symbol (&optional arg)
-  "Complete a partially typed symbol in a MATLAB mode buffer."
-  (interactive "P")
-  (if (and (featurep 'matlab-shell) (matlab-shell-active-p) matlab-shell-ask-MATLAB-for-completions)
-      ;; Use MATLAB shell if active and asking for completions is enabled.
-      (matlab-complete-symbol-with-shell arg)
-    ;; Else, do the antique version.
-    (matlab-complete-symbol-local arg)
-    ))
-
-(defun matlab-complete-symbol-with-shell (&optional arg)
-  "Complete a partially typed symbol in a MATLAB mode buffer using `matlab-shell'.
-Use `completion-in-region' to support the completion behavior."
-  (interactive "P")
-  ;; Try to do completion with the shell
-  (matlab-navigation-syntax
-    (let* ((common-substr-start-pt nil)
-	   (common-substr-end-pt nil)
-	   (prefix (if (and (not (eq last-command 'matlab-complete-symbol))
-			    (member (preceding-char) '(?  ?\t ?\n ?, ?\( ?\[ ?\')))
-		       ""
-		     (buffer-substring-no-properties
-		      (save-excursion (forward-word -1) (setq common-substr-start-pt (point)))
-		      (setq common-substr-end-pt (point)))))
-	   (completion-info (matlab-shell-completion-list prefix))
-           (completions (cdr (assoc 'completions completion-info)))
-	   )
-      (completion-in-region common-substr-start-pt common-substr-end-pt completions)
-      ))
-  )
-  
-(defun matlab-complete-symbol-local (&optional arg)
-  "Complete a partially typed symbol in a MATLAB mode buffer.
-If the previously entered command was also `matlab-complete-symbol'
-then undo the last completion, and find a new one.
-  The types of symbols tried are based on the semantics of the current
+The types of symbols tried are based on the semantics of the current
 cursor position.  There are two types of symbols.  For example, if the
 cursor is in an if statement, boolean style functions and symbols are
 tried first.  If the line is blank, then flow control, or high level
@@ -443,102 +406,59 @@ just want a completion list once, then use the universal argument ARG
 to change it temporarily."
   (interactive "P")
   (matlab-navigation-syntax
-    (let* ((prefix (if (and (not (eq last-command 'matlab-complete-symbol))
-			    (member (preceding-char) '(?  ?\t ?\n ?, ?\( ?\[ ?\')))
+    (let* ((common-substr-start-pt nil)
+	   (common-substr-end-pt nil)
+	   (prefix (if (member (preceding-char) '(?  ?\t ?\n ?, ?\( ?\[ ?\'))
 		       ""
 		     (buffer-substring-no-properties
-		      (save-excursion (forward-word -1) (point))
-		      (point))))
+		      (save-excursion (forward-word -1) (setq common-substr-start-pt (point)))
+		      (setq common-substr-end-pt (point)))))
 	   (sem (matlab-lattr-semantics prefix))
-	   (matlab-completion-technique
-	    (if arg (cond ((eq matlab-completion-technique 'complete)
-			   'increment)
-			  (t 'complete))
-	      matlab-completion-technique)))
-      (if (not (eq last-command 'matlab-complete-symbol))
-	  (setq matlab-last-prefix prefix
-		matlab-last-semantic sem
-		matlab-completion-search-state
-		(cond ((eq sem 'solo)
-		       '(matlab-solo-completions
-			 matlab-find-user-functions
-			 matlab-find-recent-variable))
-		      ((eq sem 'boolean)
-		       '(matlab-find-recent-variable
-			 matlab-boolean-completions
-			 matlab-find-user-functions
-			 matlab-value-completions))
-		      ((eq sem 'value)
-		       '(matlab-find-recent-variable
-			 matlab-find-user-functions
-			 matlab-value-completions
-			 matlab-boolean-completions))
-		      ((eq sem 'property)
-		       '(matlab-property-completions
-			 matlab-find-user-functions
-			 matlab-find-recent-variable
-			 matlab-value-completions))
-		      (t '(matlab-find-recent-variable
-			   matlab-find-user-functions
-			   matlab-value-completions
-			   matlab-boolean-completions)))))
-      (cond
-       ((eq matlab-completion-technique 'increment)
-	(let ((r nil) (donext (eq last-command 'matlab-complete-symbol)))
-	  (while (and (not r) matlab-completion-search-state)
-	    (message "Expand with %S" (car matlab-completion-search-state))
-	    (setq r (funcall (car matlab-completion-search-state)
-			     matlab-last-prefix donext))
-	    (if (not r) (setq matlab-completion-search-state
-			      (cdr matlab-completion-search-state)
-			      donext nil)))
-	  (delete-region (point) (progn (forward-char (- (length prefix)))
-					(point)))
-	  (if r
-	      (insert r)
-	    (insert matlab-last-prefix)
-	    (message "No completions."))))
-       ((eq matlab-completion-technique 'complete)
-	(let ((allsyms (apply 'append
-			      (mapcar (lambda (f) (funcall f prefix))
-				      matlab-completion-search-state))))
-	  (cond ((null allsyms)
-		 (message "No completions.")
-		 (ding))
-		((= (length allsyms) 1)
-		 (delete-region (point) (progn
-					  (forward-char (- (length prefix)))
-					  (point)))
-		 (insert (car allsyms)))
-		((= (length allsyms) 0)
-		 (message "No completions."))
-		(t
-		 (let* ((al (mapcar (lambda (a) (list a)) allsyms))
-			(c (try-completion prefix al)))
-		   ;; This completion stuff lets us expand as much as is
-		   ;; available to us. When the completion is the prefix
-		   ;; then we want to display all the strings we've
-		   ;; encountered.
-		   (if (and (stringp c) (not (string= prefix c)))
-		       (progn
-			 (delete-region
-			  (point)
-			  (progn (forward-char (- (length prefix)))
-				 (point)))
-			 (insert c))
-		     ;; `display-completion-list' does all the complex
-		     ;; ui work for us.
-		     (with-output-to-temp-buffer "*Completions*"
-		       (display-completion-list
-			(matlab-uniquify-list allsyms)))))))))))))
+	   (search-state (cond ((eq sem 'solo)
+				'(matlab-solo-completions
+				  matlab-find-user-functions
+				  matlab-find-recent-variable))
+			       ((eq sem 'boolean)
+				'(matlab-find-recent-variable
+				  matlab-boolean-completions
+				  matlab-find-user-functions
+				  matlab-value-completions))
+			       ((eq sem 'value)
+				'(matlab-find-recent-variable
+				  matlab-find-user-functions
+				  matlab-value-completions
+				  matlab-boolean-completions))
+			       ((eq sem 'property)
+				'(matlab-property-completions
+				  matlab-find-user-functions
+				  matlab-find-recent-variable
+				  matlab-value-completions))
+			       (t '(matlab-find-recent-variable
+				    matlab-find-user-functions
+				    matlab-value-completions
+				    matlab-boolean-completions))))
+	   (r nil)
+	   )
+      (while search-state
+	(setq r (append r (funcall (car search-state) prefix))
+	      search-state (cdr search-state))
+	)
+      
+      (list common-substr-start-pt common-substr-end-pt r))))
 
+(defun matlab-completions-local-only (prefix)
+  "Use local completion engine to get the list of completions for local entities.
+Used by shell completion where the shell can't see inside the current file."
+  (let ((search-state '(matlab-find-user-functions
+			matlab-find-recent-variable))
+	r)
+    
+    (while search-state
+      (setq r (append r (funcall (car search-state) prefix))
+	    search-state (cdr search-state)))
+
+    r))
 
 (provide 'matlab-complete)
 
 ;;; matlab-complete.el ends here
-
-;; LocalWords:  el Ludlam zappo ish defcustom CLim XColor XDir XLabel
-;; LocalWords:  XAxis XScale YColor YDir YAxis YScale YTick ZColor ZDir ZGrid
-;; LocalWords:  ZLabel ZScale ZTick Dithermap defun lst tl setq cdr defmacro
-;; LocalWords:  nreverse eol progn foundlst expandto listp stringp sem lattr
-;; LocalWords:  donext funcall allsyms mapcar
